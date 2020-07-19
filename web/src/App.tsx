7cda0acafd5339c669bfa4efa270f6b7f6d17e7c
Bob Glickstein <bobg@emphatic.com>
2020-07-19 12:39:36 -0700

    Use react-bootstrap's provided CSS.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { Button, Card, Spinner } from 'react-bootstrap'
 
 import { Alert, doAlert, setAlertRef } from './Alert'
