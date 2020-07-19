8a514bc2e4510bae8e782a87b2d970e70676711d
Bob Glickstein <bobg@emphatic.com>
2020-07-19 12:54:52 -0700

    Use a stylesheet link to the bootstrap CDN.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { Button, Card, Spinner } from 'react-bootstrap'
 
 import { Alert, doAlert, setAlertRef } from './Alert'
